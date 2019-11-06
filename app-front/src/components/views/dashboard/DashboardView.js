4d07f6a870f76001efcaf2a51144699ff363549d
Nadir Si Mohammed <nadir.si-mohammed@etu.upec.fr>
2019-11-06 16:41:37 +0100

    dahbord View done , 3 warnnings to fix !!

--- app-front/src/components/views/dashboard/DashboardView.js
+++ app-front/src/components/views/dashboard/DashboardView.js
@@ -1,9 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 import ListItem from "@material-ui/core/ListItem";
 import List from "@material-ui/core/List";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
@@ -12,7 +9,6 @@ import ErrorIcon from "@material-ui/icons/Error";
 import IssueItem from "./IssueItem";
 
 function DashboardView() {
-  const [extended, setExtended] = useState(false);
   const issues = issuesListResult.result;
 
   return (
@@ -25,32 +21,20 @@ function DashboardView() {
       <Typography variant="h1" component="h1" gutterBottom>
         All Issues
       </Typography>
-      <FormGroup row>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={extended}
-              onChange={event => setExtended(event.target.checked)}
-              value="secondary"
-            />
-          }
-          label="Enable secondary text"
-        />
-      </FormGroup>
-      <Grid item xs={6}>
+      <Grid item xs={12}>
         <List style={{ backgroundColor: "white" }}>
           <ListItem divider>
             <ListItemIcon>
-              <ErrorIcon style={{ color: "green" }} />
+              <ErrorIcon style={{ marginLeft: 5, color: "green" }} />
               <Typography>39 Issues Opened</Typography>
             </ListItemIcon>
             <ListItemIcon>
-              <ErrorIcon style={{ color: "red" }} />
+              <ErrorIcon style={{ marginLeft: 5, color: "red" }} />
               <Typography>39 Issues Opened</Typography>
             </ListItemIcon>
           </ListItem>
           {issues.map(issue => (
-            <IssueItem key={issue.id} extended={extended} issue={issue} />
+            <IssueItem key={issue.id} issue={issue} />
           ))}
         </List>
       </Grid>
@@ -64,18 +48,37 @@ const issuesListResult = {
   result: [
     {
       id: "123FASDWE",
+      createdAt: "2019-09-05T14:24:21Z",
+      updatedAt: "2019-10-05T14:24:21Z",
       title: "CMC calculation error",
       creator: { id: "123123", name: "diallo" },
       status: { id: "1213DEF", name: "open" },
+      open: false,
       assignedUsers: [
-        { id: "123123123", name: "diallo" },
-        { id: "123123123", name: "nadir" }
+        { id: "123123125", name: "diallo" },
+        { id: "123123124", name: "nadir" }
       ],
       tags: [
-        { id: "123SFQ", name: "Devops", color: "#12FFEE" },
+        { id: "123SFF", name: "Devops", color: "#12FFEE" },
         { id: "123SFQ", name: "Bug", color: "#12FFAA" }
       ],
       project: { name: "bug Manager 1" }
+    },
+    {
+      id: "223FASDWZ",
+      title: "CMC calculation error 2",
+      creator: { id: "223124", name: "Safiy" },
+      status: { id: "2213DEF", name: "on going" },
+      open: true,
+      assignedUsers: [
+        { id: "223123125", name: "Ludwig" },
+        { id: "223123124", name: "nadir" }
+      ],
+      tags: [
+        { id: "223SFF", name: "Devops", color: "#12FFEE" },
+        { id: "224SFQ", name: "Bug", color: "#12FFAA" }
+      ],
+      project: { name: "bug Manager 2" }
     }
   ]
 };
--- app-front/src/components/views/dashboard/IssueItem.js
+++ app-front/src/components/views/dashboard/IssueItem.js
@@ -1,14 +1,40 @@
 import React from "react";
 import PropType from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
 import { useHistory } from "react-router-dom";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ErrorIcon from "@material-ui/icons/Error";
 import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import Chip from "@material-ui/core/Chip";
+import moment from "moment";
+import { invertColor } from "../../core/utils/Functions";
 
-function IssueC(props) {
-  const { extended, issue } = props;
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    backgroundColor: theme.palette.background.paper
+  },
+  inline: {
+    display: "inline",
+    marginLeft: 5,
+    fontSize: 15
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  description: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary
+  }
+}));
+
+function IssueItem(props) {
+  const { issue } = props;
   const history = useHistory();
+  const classes = useStyles();
 
   return (
     <ListItem
@@ -18,19 +44,78 @@ function IssueC(props) {
       alignItems="flex-start"
     >
       <ListItemIcon>
-        <ErrorIcon style={{ color: "red" }} />
+        <ErrorIcon style={{ color: issue.open ? "green" : "red" }} />
       </ListItemIcon>
       <ListItemText
-        primary={issue.title}
-        secondary={extended ? issue.title : null}
+        primary={
+          <Typography
+            component="span"
+            variant="body"
+            className={classes.title}
+            color="textPrimary"
+          >
+            {issue.title}
+          </Typography>
+        }
+        secondary={
+          <>
+            <Typography
+              component="span"
+              variant="body"
+              className={classes.inline}
+              color="textPrimary"
+            >
+              Created {moment(issue.createdAt).fromNow()} by{" "}
+              {issue.creator.name}
+            </Typography>
+            {" - "}
+            <Typography
+              component="span"
+              variant="body2"
+              className={classes.inline}
+              color="textPrimary"
+            >
+              last Updated {moment(issue.updatedAt).fromNow()}
+            </Typography>
+            <Typography className={classes.inline}>
+              {" - Assigned to "}{" "}
+              {issue.assignedUsers.map(user => (
+                <>
+                  {user.name}
+                  {", "}
+                </>
+              ))}
+            </Typography>
+            <Typography
+              component="span"
+              variant="body"
+              className={classes.inline}
+              color="textPrimary"
+            >
+              {" - this issue opened on "} {issue.project.name}
+            </Typography>
+            {issue.tags.map(tag => (
+              <Chip
+                key={tag.id}
+                size="small"
+                label={tag.name}
+                style={{
+                  borderColor: tag.color,
+                  marginLeft: 5,
+                  backgroundColor: tag.color,
+                  color: invertColor(tag.color)
+                }}
+              />
+            ))}
+          </>
+        }
       />
     </ListItem>
   );
 }
 
-IssueC.propTypes = {
-  issue: PropType.object.isRequired,
-  extended: PropType.object.isRequired
+IssueItem.propTypes = {
+  issue: PropType.object.isRequired
 };
 
-export default IssueC;
+export default IssueItem;
