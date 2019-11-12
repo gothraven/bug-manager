5654ed191a2b9e7fad8bd0a24c464d9676af3b76
Safiy Errahmane ZAGHBANE <szaghbane@gmail.com>
2019-11-13 00:45:37 +0100

    Add forgoten import after back-merge

--- app-front/src/components/layouts/main/MainLayout.js
+++ app-front/src/components/layouts/main/MainLayout.js
@@ -1,9 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Suspense } from "react";
+import PropTypes from "prop-types";}
 import { makeStyles } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import CircularProgress from "@material-ui/core/CircularProgress";
-
 import SideBar from "./SideBar";
 
 const useStyles = makeStyles(theme => ({
