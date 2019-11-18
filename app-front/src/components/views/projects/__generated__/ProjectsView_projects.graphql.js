2e2368b43eb4837eb2086c25a96480279641e500
Safiy Errahmane ZAGHBANE <szaghbane@gmail.com>
2019-11-19 00:14:15 +0100

    Make sure both eslint and prettier ignores graphql
    - AUTO generated files *.graphql.js should be ignored by linters and
    formatters

--- app-front/src/components/views/projects/__generated__/ProjectsView_projects.graphql.js
+++ app-front/src/components/views/projects/__generated__/ProjectsView_projects.graphql.js
@@ -4,7 +4,7 @@
 
 /* eslint-disable */
 
-"use strict";
+'use strict';
 
 /*::
 import type { ReaderFragment } from 'relay-runtime';
@@ -30,144 +30,147 @@ export type ProjectsView_projects$key = {
 };
 */
 
-const node /*: ReaderFragment*/ = (function() {
-  var v0 = ["projects"];
-  return {
-    kind: "Fragment",
-    name: "ProjectsView_projects",
-    type: "Query",
-    metadata: {
-      connection: [
-        {
-          count: "first",
-          cursor: "after",
-          direction: "forward",
-          path: (v0 /*: any*/)
-        }
-      ],
-      refetch: {
-        connection: {
-          forward: {
-            count: "first",
-            cursor: "after"
-          },
-          backward: null,
-          path: (v0 /*: any*/)
-        },
-        operation: require("./ProjectsPaginationQuery.graphql.js"),
-        fragmentPathInResult: []
-      }
-    },
-    argumentDefinitions: [
+
+const node/*: ReaderFragment*/ = (function(){
+var v0 = [
+  "projects"
+];
+return {
+  "kind": "Fragment",
+  "name": "ProjectsView_projects",
+  "type": "Query",
+  "metadata": {
+    "connection": [
       {
-        kind: "LocalArgument",
-        name: "first",
-        type: "PositiveInt",
-        defaultValue: 10
-      },
-      {
-        kind: "LocalArgument",
-        name: "after",
-        type: "String",
-        defaultValue: ""
+        "count": "first",
+        "cursor": "after",
+        "direction": "forward",
+        "path": (v0/*: any*/)
       }
     ],
-    selections: [
-      {
-        kind: "LinkedField",
-        alias: "projects",
-        name: "__Query_projects_connection",
-        storageKey: null,
-        args: null,
-        concreteType: "ProjectCursor",
-        plural: false,
-        selections: [
-          {
-            kind: "LinkedField",
-            alias: null,
-            name: "edges",
-            storageKey: null,
-            args: null,
-            concreteType: "ProjectEdge",
-            plural: true,
-            selections: [
-              {
-                kind: "LinkedField",
-                alias: null,
-                name: "node",
-                storageKey: null,
-                args: null,
-                concreteType: "Project",
-                plural: false,
-                selections: [
-                  {
-                    kind: "ScalarField",
-                    alias: null,
-                    name: "id",
-                    args: null,
-                    storageKey: null
-                  },
-                  {
-                    kind: "ScalarField",
-                    alias: null,
-                    name: "name",
-                    args: null,
-                    storageKey: null
-                  },
-                  {
-                    kind: "ScalarField",
-                    alias: null,
-                    name: "description",
-                    args: null,
-                    storageKey: null
-                  },
-                  {
-                    kind: "ScalarField",
-                    alias: null,
-                    name: "__typename",
-                    args: null,
-                    storageKey: null
-                  }
-                ]
-              },
-              {
-                kind: "ScalarField",
-                alias: null,
-                name: "cursor",
-                args: null,
-                storageKey: null
-              }
-            ]
-          },
-          {
-            kind: "LinkedField",
-            alias: null,
-            name: "pageInfo",
-            storageKey: null,
-            args: null,
-            concreteType: "PageInfo",
-            plural: false,
-            selections: [
-              {
-                kind: "ScalarField",
-                alias: null,
-                name: "endCursor",
-                args: null,
-                storageKey: null
-              },
-              {
-                kind: "ScalarField",
-                alias: null,
-                name: "hasNextPage",
-                args: null,
-                storageKey: null
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  };
+    "refetch": {
+      "connection": {
+        "forward": {
+          "count": "first",
+          "cursor": "after"
+        },
+        "backward": null,
+        "path": (v0/*: any*/)
+      },
+      "operation": require('./ProjectsPaginationQuery.graphql.js'),
+      "fragmentPathInResult": []
+    }
+  },
+  "argumentDefinitions": [
+    {
+      "kind": "LocalArgument",
+      "name": "first",
+      "type": "PositiveInt",
+      "defaultValue": 10
+    },
+    {
+      "kind": "LocalArgument",
+      "name": "after",
+      "type": "String",
+      "defaultValue": ""
+    }
+  ],
+  "selections": [
+    {
+      "kind": "LinkedField",
+      "alias": "projects",
+      "name": "__Query_projects_connection",
+      "storageKey": null,
+      "args": null,
+      "concreteType": "ProjectCursor",
+      "plural": false,
+      "selections": [
+        {
+          "kind": "LinkedField",
+          "alias": null,
+          "name": "edges",
+          "storageKey": null,
+          "args": null,
+          "concreteType": "ProjectEdge",
+          "plural": true,
+          "selections": [
+            {
+              "kind": "LinkedField",
+              "alias": null,
+              "name": "node",
+              "storageKey": null,
+              "args": null,
+              "concreteType": "Project",
+              "plural": false,
+              "selections": [
+                {
+                  "kind": "ScalarField",
+                  "alias": null,
+                  "name": "id",
+                  "args": null,
+                  "storageKey": null
+                },
+                {
+                  "kind": "ScalarField",
+                  "alias": null,
+                  "name": "name",
+                  "args": null,
+                  "storageKey": null
+                },
+                {
+                  "kind": "ScalarField",
+                  "alias": null,
+                  "name": "description",
+                  "args": null,
+                  "storageKey": null
+                },
+                {
+                  "kind": "ScalarField",
+                  "alias": null,
+                  "name": "__typename",
+                  "args": null,
+                  "storageKey": null
+                }
+              ]
+            },
+            {
+              "kind": "ScalarField",
+              "alias": null,
+              "name": "cursor",
+              "args": null,
+              "storageKey": null
+            }
+          ]
+        },
+        {
+          "kind": "LinkedField",
+          "alias": null,
+          "name": "pageInfo",
+          "storageKey": null,
+          "args": null,
+          "concreteType": "PageInfo",
+          "plural": false,
+          "selections": [
+            {
+              "kind": "ScalarField",
+              "alias": null,
+              "name": "endCursor",
+              "args": null,
+              "storageKey": null
+            },
+            {
+              "kind": "ScalarField",
+              "alias": null,
+              "name": "hasNextPage",
+              "args": null,
+              "storageKey": null
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
 })();
 // prettier-ignore
 (node/*: any*/).hash = '1926d6a2e9bf9f2f606ceac2498a5a34';
