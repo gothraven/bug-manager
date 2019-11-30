d94e596a97d75fbbc8438816f442de92d945cf91
Safiy Errahmane ZAGHBANE <szaghbane@gmail.com>
2019-11-30 20:17:39 +0100

    Downgrade from relay to simple cursor pagination
    - Since we're moving back to apollo to simplify things
    it is better to simplify the pagination as well !

--- core-api/src/api/graphql/resolvers/pagination.resolver.js
+++ core-api/src/api/graphql/resolvers/pagination.resolver.js
@@ -15,30 +15,13 @@ export const Paginate = model => async (parent, { after, first = 100 }, { models
     limit: first + 1
   });
   const hasNextPage = data.length > first;
-  let edges = hasNextPage ? data.slice(0, -1) : data;
-  const totalCount = edges.length;
-  edges = edges.map((node) => {
-    const endCursor = toCursorHash(node.createdAt.toString());
-    return {
-      cursor: endCursor,
-      node
-    };
-  });
-  const startCursor = edges.length > 0 ? edges[0].cursor : null;
+  const edges = hasNextPage ? data.slice(0, -1) : data;
   const endCursor = edges.length > 0 ? edges[edges.length - 1].cursor : null;
   return {
     edges,
     pageInfo: {
       hasNextPage,
-      hasPreviousPage: false,
-      endCursor,
-      startCursor
-    },
-    totalCount
+      endCursor
+    }
   };
-};
-
-export const Edgify = () => async (parent) => {
-  const cursor = toCursorHash(parent.createdAt.toString());
-  return { cursor, node: parent };
 };
