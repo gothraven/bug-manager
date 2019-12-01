fa76ce5ae9fd665719bcaa6035026d3856625b82
Safiy Errahmane ZAGHBANE <szaghbane@gmail.com>
2019-12-01 02:10:04 +0100

    Replace Relay with Apollo on Dashboard Page

---
+++ app-front/src/components/views/dashboard/DashboardView.scss.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  listHeaderBox: {
+    display: "grid",
+    gridTemplateColumns: "1fr 4fr",
+    alignItems: "center"
+  }
+}));
