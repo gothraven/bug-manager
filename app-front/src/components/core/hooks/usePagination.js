9b530a0f15fc13d9764e17f46da64e6bc20729c1
Safiy Errahmane ZAGHBANE <szaghbane@gmail.com>
2019-12-01 01:52:22 +0100

    Add new hooks to our collection
    - Stop using useMutation since Apollo already implements it
    - Create usePagination hook for pagination
    - Create useMe hook to fetch Me from the back-end but later it will move

---
+++ app-front/src/components/core/hooks/usePagination.js
@@ -0,0 +1,38 @@
+import { useCallback } from "react";
+import { useQuery } from '@apollo/react-hooks';
+
+
+export default function usePagination(query, model) {
+  const { data, error, loading, fetchMore } = useQuery(query);
+
+  const loadMore = useCallback(() => {
+    const { endCursor, hasNextPage } = data.tags.pageInfo;
+
+    if (loading || !hasNextPage) {
+      return;
+    }
+
+    fetchMore({
+      variables: {
+        cursor: endCursor
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        const newEdges = fetchMoreResult[model].edges;
+        const { pageInfo } = fetchMoreResult[model];
+
+        return newEdges.length
+          ? {
+            [model]: {
+              __typename: previousResult[model].__typename,
+              edges: [...previousResult[model].edges, ...newEdges],
+              pageInfo,
+            },
+          }
+          : previousResult;
+      }
+    });
+
+  }, [data, loading, fetchMore, model]);
+
+  return { data, error, loading, fetchMore: loadMore }
+}
