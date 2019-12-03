9f4f63c2a23ebaba6f91dacd1dde3d4b58df4af9
Safiy Errahmane ZAGHBANE <szaghbane@gmail.com>
2019-12-03 02:16:18 +0100

    Add Can to handle permisions based on role
    - Fixed SideBar
    - Fixed TagsView
    - Fixed ProjectView

--- app-front/src/components/views/projects/ProjectsView.js
+++ app-front/src/components/views/projects/ProjectsView.js
@@ -1,22 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useMutation } from "@apollo/react-hooks";
-import propTypes from "prop-types";
 import Fab from "@material-ui/core/Fab";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import ProjectCard from "./ProjectCard";
-import { ADMIN } from "../../core/constants";
 import {
   PROJECTS_QUERY,
   CREATE_PROJECT
 } from "../../core/models/projects/projects.graphql";
+import { Can, AbilityContext } from "../../core/Ability";
 import { usePagination } from "../../core/hooks";
 import Loading from "../../lib/Loading";
 
-function ProjectsView(props) {
-  const { me } = props;
+function ProjectsView() {
+  const ability = useContext(AbilityContext)
   const { data, loading: loadingProjects, fetchMore } = usePagination(
     PROJECTS_QUERY,
     "projects"
@@ -65,43 +64,32 @@ function ProjectsView(props) {
             }
             return (
               <Grid key={node.id} item xs={4}>
-                <ProjectCard disabled={me.role !== ADMIN} project={node} />
+                <ProjectCard disabled={!ability.can("edit", "Project")} project={node} />
               </Grid>
             );
           })}
         </Grid>
       </Grid>
       {hasNextPage && <Button onClick={fetchMore}>load more</Button>}
-      {me.role === ADMIN && (
-        <Grid
-          item
-          style={{ display: "grid", justifyContent: "center", padding: 30 }}
-        >
-          <Fab
-            color="primary"
-            aria-label="add"
-            onClick={onCreateProject}
-            disabled={isProjectCreatePending}
+      <Can I="create" a="Project">
+        {() => (
+          <Grid
+            item
+            style={{ display: "grid", justifyContent: "center", padding: 30 }}
           >
-            <AddIcon />
-          </Fab>
-        </Grid>
-      )}
+            <Fab
+              color="primary"
+              aria-label="add"
+              onClick={onCreateProject}
+              disabled={isProjectCreatePending}
+            >
+              <AddIcon />
+            </Fab>
+          </Grid>
+        )}
+      </Can>
     </Grid>
   );
 }
-
-ProjectsView.defaultProps = {
-  me: null
-};
-
-ProjectsView.propTypes = {
-  me: propTypes.shape({
-    id: propTypes.string,
-    name: propTypes.string,
-    email: propTypes.string,
-    role: propTypes.string
-  })
-};
 
 export default ProjectsView;
