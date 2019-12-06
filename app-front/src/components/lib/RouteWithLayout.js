9e0ecdb1c6dbb5c4754fd7c40f728b49b87d22f8
Safiy Errahmane ZAGHBANE <szaghbane@gmail.com>
2019-12-06 02:38:43 +0100

    Update routes
    - Update sign-up and sign-in to become /user/*
    - Add /user/forgot-password
    - Now when user tries to get to a covered page and got redirected to
    sign-in he gets redirected back to where he wanted to go after sign-in
    is succesfull
    - Updated RouteWithLayout and removed the Auth variable cause it is no
    longer neccessary
    - Updated copyries to point to the right url

--- app-front/src/components/lib/RouteWithLayout.js
+++ app-front/src/components/lib/RouteWithLayout.js
@@ -1,43 +1,32 @@
 import React, { useContext } from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route } from "react-router-dom";
 import PropTypes from "prop-types";
 import { AbilityContext } from "../core/Ability";
 import { NotFoundView } from "../views";
 
 function RouteWithLayout(props) {
-  const { authed, layout: Layout, component: Component, name, ...rest } = props;
+  const { layout: Layout, component: Component, name, ...rest } = props;
   const ability = useContext(AbilityContext);
 
   return (
     <Route
       {...rest}
       render={matchProps =>
-        authed === true ? (
-          <Layout>
-            {
-              ability.can("see", name)
-                ? <Component {...matchProps} />
-                : <NotFoundView />
-            }
-          </Layout>
-        ) : (
-            <Redirect
-              to={{ pathname: "/sign-in", state: { from: matchProps.location } }}
-            />
-          )
+        <Layout>
+          {
+            ability.can("see", name)
+              ? <Component {...matchProps} />
+              : <NotFoundView />
+          }
+        </Layout>
       }
     />
   );
 }
 
-RouteWithLayout.default = {
-  authed: false
-};
-
 RouteWithLayout.propTypes = {
   component: PropTypes.any.isRequired,
   layout: PropTypes.any.isRequired,
-  authed: PropTypes.bool.isRequired,
   name: PropTypes.string.isRequired,
 };
 
