12ec1dc4244faa2fa98138c35095d347a7a4f689
Safiy Errahmane ZAGHBANE <szaghbane@gmail.com>
2020-01-06 17:12:07 +0100

    Add two script for project testing

--- core-api/populate.db.js
+++ core-api/populate.db.js
@@ -42,53 +42,56 @@ db.once('open', async () => {
   await Promise.all(defaultUsers.map(async (user) => {
     await models.User.create(user);
   }));
-  await Promise.all(_.range(7).map(async () => {
-    await models.User.create({
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-      name: faker.name.findName(),
-      role: faker.random.arrayElement(['USER', 'ADMIN', 'DEVELOPER'])
-    });
-  }));
 
-  // Create Projects
-  await Promise.all(_.range(5).map(async () => {
-    await models.Project.create({
-      name: faker.commerce.productName(),
-      description: faker.lorem.sentence()
-    });
-  }));
+  if (process.argv[2] === '--full') {
+    await Promise.all(_.range(7).map(async () => {
+      await models.User.create({
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        name: faker.name.findName(),
+        role: faker.random.arrayElement(['USER', 'ADMIN', 'DEVELOPER'])
+      });
+    }));
 
-  // Create Tags
-  await Promise.all(defaultTags.map(async (value) => {
-    await models.Tag.create({
-      name: value,
-      description: faker.lorem.sentence(),
-      color: faker.internet.color().toUpperCase()
-    });
-  }));
+    // Create Projects
+    await Promise.all(_.range(5).map(async () => {
+      await models.Project.create({
+        name: faker.commerce.productName(),
+        description: faker.lorem.sentence()
+      });
+    }));
 
-  // Create Statuses
-  await Promise.all(defaultStatuses.map(async (value) => {
-    await models.Status.create({
-      name: value,
-      description: faker.lorem.sentence()
-    });
-  }));
+    // Create Tags
+    await Promise.all(defaultTags.map(async (value) => {
+      await models.Tag.create({
+        name: value,
+        description: faker.lorem.sentence(),
+        color: faker.internet.color().toUpperCase()
+      });
+    }));
 
-  // Create Issues
-  const users = await models.User.find({});
-  await Promise.all(_.range(15).map(async () => {
-    const creatorId = faker.random.arrayElement(users).id;
-    const issue = await models.Issue.create({
-      title: faker.company.catchPhrase(),
-      creatorId
-    });
-    await models.Comment.create({
-      creatorId,
-      issueId: issue.id,
-      content: faker.lorem.paragraph()
-    });
-  }));
+    // Create Statuses
+    await Promise.all(defaultStatuses.map(async (value) => {
+      await models.Status.create({
+        name: value,
+        description: faker.lorem.sentence()
+      });
+    }));
+
+    // Create Issues
+    const users = await models.User.find({});
+    await Promise.all(_.range(15).map(async () => {
+      const creatorId = faker.random.arrayElement(users).id;
+      const issue = await models.Issue.create({
+        title: faker.company.catchPhrase(),
+        creatorId
+      });
+      await models.Comment.create({
+        creatorId,
+        issueId: issue.id,
+        content: faker.lorem.paragraph()
+      });
+    }));
+  }
   process.exit();
 });
